8e3970277c7cb388291c1e0415df8402bb6c3f7e
ChengXiao686 <xiao.cheng@autra.tech>
2023-09-18 21:19:39 +0800

    add some styles

---
+++ modules/sdxl_styles_c.py
@@ -0,0 +1,210 @@
+styles = [
+    {
+        "name": "Default (Slightly Cinematic)",
+        "c_name": "默认（略带电影感）",
+        "prompt": "cinematic still {prompt} . emotional, harmonious, vignette, highly detailed, high budget, bokeh, cinemascope, moody, epic, gorgeous, film grain, grainy",
+        "c_prompt": "电影画面 {prompt} . 情感丰富，和谐，小插图，高度详细，高预算，背景虚化，电影宽幕，情感丰富，史诗，华丽，胶片颗粒，颗粒感",
+        "negative_prompt": "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
+        "c_negative_prompt": "动漫，卡通，图形，文本，绘画，蜡笔，石墨，抽象，故障，变形，突变，丑陋，毁容"
+    },
+    {
+        "name": "sai-3d-model",
+        "c_name": "sai-3D模型",
+        "prompt": "professional 3d model {prompt} . octane render, highly detailed, volumetric, dramatic lighting",
+        "c_prompt": "专业3D模型 {prompt} . 八面体渲染，高度详细，体积，戏剧性照明",
+        "negative_prompt": "ugly, deformed, noisy, low poly, blurry, painting",
+        "c_negative_prompt": "丑陋，变形，嘈杂，低多边形，模糊，绘画"
+    },
+    {
+        "name": "sai-analog film",
+        "c_name": "sai-胶片",
+        "prompt": "analog film photo {prompt} . faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage",
+        "c_prompt": "胶片照片 {prompt} . 褪色的胶片，去饱和，35mm照片，颗粒感，小插图，复古，柯达彩色，洛莫，污渍，高度详细，找到的素材",
+        "negative_prompt": "painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured",
+        "c_negative_prompt": "绘画，图画，插图，故障，变形，突变，斗鸡眼，丑陋，毁容"
+    },
+    {
+        "name": "sai-anime",
+        "c_name": "sai-动漫",
+        "prompt": "anime artwork {prompt} . anime style, key visual, vibrant, studio anime,  highly detailed",
+        "c_prompt": "动漫作品 {prompt} . 动漫风格，关键视觉，充满活力，工作室动漫，高度详细",
+        "negative_prompt": "photo, deformed, black and white, realism, disfigured, low contrast",
+        "c_negative_prompt": "照片，变形，黑白，现实主义，毁容，低对比度"
+    },
+    {
+        "name": "sai-cinematic",
+        "c_name": "sai-电影感",
+        "prompt": "cinematic film still {prompt} . shallow depth of field, vignette, highly detailed, high budget, bokeh, cinemascope, moody, epic, gorgeous, film grain, grainy",
+        "c_prompt": "电影画面 {prompt} . 浅景深，小插图，高度详细，高预算，背景虚化，电影宽幕，情感丰富，史诗，华丽，胶片颗粒，颗粒感",
+        "negative_prompt": "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
+        "c_negative_prompt": "动漫，卡通，图形，文本，绘画，蜡笔，石墨，抽象，故障，变形，突变，丑陋，毁容"
+    },
+    {
+        "name": "sai-comic book",
+        "c_name": "sai-漫画书",
+        "prompt": "comic {prompt} . graphic illustration, comic art, graphic novel art, vibrant, highly detailed",
+        "c_prompt": "漫画 {prompt} . 图形插图，漫画艺术，图像小说艺术，充满活力，高度详细",
+        "negative_prompt": "photograph, deformed, glitch, noisy, realistic, stock photo",
+        "c_negative_prompt": "照片，变形，故障，嘈杂，现实主义，库存照片"
+    },
+    {
+        "name": "sai-craft clay",
+        "c_name": "sai-手工黏土",
+        "prompt": "play-doh style {prompt} . sculpture, clay art, centered composition, Claymation",
+        "c_prompt": "橡皮泥风格 {prompt} . 雕塑，黏土艺术，居中构图，黏土动画",
+        "negative_prompt": "sloppy, messy, grainy, highly detailed, ultra textured, photo",
+        "c_negative_prompt": "马虎，凌乱，颗粒感，高度详细，超级纹理，照片"
+    },
+    {
+        "name": "sai-digital art",
+        "c_name": "sai-数字艺术",
+        "prompt": "concept art {prompt} . digital artwork, illustrative, painterly, matte painting, highly detailed",
+        "c_prompt": "概念艺术 {prompt} . 数字艺术作品，插图，绘画风格，哑光绘画，高度详细",
+        "negative_prompt": "photo, photorealistic, realism, ugly",
+        "c_negative_prompt": "照片，照片写实，现实主义，丑陋"
+    },
+    {
+        "name": "sai-enhance",
+        "c_name": "sai-增强",
+        "prompt": "breathtaking {prompt} . award-winning, professional, highly detailed",
+        "c_prompt": "惊艳 {prompt} . 获奖，专业，高度详细",
+        "negative_prompt": "ugly, deformed, noisy, blurry, distorted, grainy",
+        "c_negative_prompt": "丑陋，变形，嘈杂，模糊，扭曲，颗粒感"
+    },
+    {
+        "name": "sai-fantasy art",
+        "c_name": "sai-幻想艺术",
+        "prompt": "ethereal fantasy concept art of  {prompt} . magnificent, celestial, ethereal, painterly, epic, majestic, magical, fantasy art, cover art, dreamy",
+        "c_prompt": "空灵的幻想概念艺术 {prompt} . 壮丽，天体，空灵，绘画风格，史诗，雄伟，神奇，幻想艺术，封面艺术，梦幻",
+        "negative_prompt": "photographic, realistic, realism, 35mm film, dslr, cropped, frame, text, deformed, glitch, noise, noisy, off-center, deformed, cross-eyed, closed eyes, bad anatomy, ugly, disfigured, sloppy, duplicate, mutated, black and white",
+        "c_negative_prompt": "摄影，现实主义，现实，35mm胶片，单反相机，裁剪，框架，文本，变形，故障，噪音，嘈杂，偏心，变形，斗鸡眼，闭眼，不良解剖学，丑陋，毁容，马虎，重复，突变，黑白"
+    },
+    {
+        "name": "sai-isometric",
+        "c_name": "sai-等角",
+        "prompt": "isometric style {prompt} . vibrant, beautiful, crisp, detailed, ultra detailed, intricate",
+        "c_prompt": "等角风格 {prompt} . 充满活力，美丽，清晰，详细，超级详细，复杂",
+        "negative_prompt": "deformed, mutated, ugly, disfigured, blur, blurry, noise, noisy, realistic, photographic",
+        "c_negative_prompt": "变形，突变，丑陋，毁容，模糊，嘈杂，现实主义，摄影"
+    },
+    {
+        "name": "sai-line art",
+        "c_name": "sai-线条艺术",
+        "prompt": "line art drawing {prompt} . professional, sleek, modern, minimalist, graphic, line art, vector graphics",
+        "c_prompt": "线条艺术绘图 {prompt} . 专业，流畅，现代，简约，图形，线条艺术，矢量图形",
+        "negative_prompt": "anime, photorealistic, 35mm film, deformed, glitch, blurry, noisy, off-center, deformed, cross-eyed, closed eyes, bad anatomy, ugly, disfigured, mutated, realism, realistic, impressionism, expressionism, oil, acrylic",
+        "c_negative_prompt": "动漫，照片写实，35mm胶片，变形，故障，模糊，嘈杂，偏心，变形，斗鸡眼，闭眼，不良解剖学，丑陋，毁容，突变，现实主义，现实，印象主义，表现主义，油画，丙烯"
+    },
+    {
+        "name": "sai-lowpoly",
+        "c_name": "sai-低多边形",
+        "prompt": "low-poly style {prompt} . low-poly game art, polygon mesh, jagged, blocky, wireframe edges, centered composition",
+        "c_prompt": "低多边形风格 {prompt} . 低多边形游戏艺术，多边形网格，参差不齐，块状，线框边缘，居中构图",
+        "negative_prompt": "noisy, sloppy, messy, grainy, highly detailed, ultra textured, photo",
+        "c_negative_prompt": "嘈杂，马虎，凌乱，颗粒感，高度详细，超级纹理，照片"
+    },
+    {
+        "name": "sai-neonpunk",
+        "c_name": "sai-霓虹朋克",
+        "prompt": "neonpunk style {prompt} . cyberpunk, vaporwave, neon, vibes, vibrant, stunningly beautiful, crisp, detailed, sleek, ultramodern, magenta highlights, dark purple shadows, high contrast, cinematic, ultra detailed, intricate, professional",
+        "c_prompt": "霓虹朋克风格 {prompt} . 赛博朋克，蒸汽波，霓虹，氛围，充满活力，惊艳美丽，清晰，详细，流畅，超现代，洋红色高光，深紫色阴影，高对比度，电影感，超级详细，复杂，专业",
+        "negative_prompt": "painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured",
+        "c_negative_prompt": "绘画，图画，插图，故障，变形，突变，斗鸡眼，丑陋，毁容"
+    },
+    {
+        "name": "sai-origami",
+        "c_name": "sai-折纸",
+        "prompt": "origami style {prompt} . paper art, pleated paper, folded, origami art, pleats, cut and fold, centered composition",
+        "c_prompt": "折纸风格 {prompt} . 纸艺术，褶皱纸，折叠，折纸艺术，褶皱，剪切和折叠，居中构图",
+        "negative_prompt": "noisy, sloppy, messy, grainy, highly detailed, ultra textured, photo",
+        "c_negative_prompt": "嘈杂，马虎，凌乱，颗粒感，高度详细，超级纹理，照片"
+    },
+    {
+        "name": "sai-photographic",
+        "c_name": "sai-摄影",
+        "prompt": "cinematic photo {prompt} . 35mm photograph, film, bokeh, professional, 4k, highly detailed",
+        "c_prompt": "电影感照片 {prompt} . 35mm照片，胶片，背景虚化，专业，4K，高度详细",
+        "negative_prompt": "drawing, painting, crayon, sketch, graphite, impressionist, noisy, blurry, soft, deformed, ugly",
+        "c_negative_prompt": "绘画，油画，蜡笔，素描，石墨，印象派，嘈杂，模糊，柔和，变形，丑陋"
+    },
+    {
+        "name": "sai-pixel art",
+        "c_name": "sai-像素艺术",
+        "prompt": "pixel-art {prompt} . low-res, blocky, pixel art style, 8-bit graphics",
+        "c_prompt": "像素艺术 {prompt} . 低分辨率，块状，像素艺术风格，8位图形",
+        "negative_prompt": "sloppy, messy, blurry, noisy, highly detailed, ultra textured, photo, realistic",
+        "c_negative_prompt": "马虎，凌乱，模糊，嘈杂，高度详细，超级纹理，照片，现实主义"
+    },
+    {
+        "name": "sai-texture",
+        "c_name": "sai-纹理",
+        "prompt": "texture {prompt} top down close-up",
+        "c_prompt": "纹理 {prompt} 从上到下的特写",
+        "negative_prompt": "ugly, deformed, noisy, blurry",
+        "c_negative_prompt": "丑陋，变形，嘈杂，模糊"
+    },
+    {
+        "name": "ads-advertising",
+        "c_name": "ads-广告",
+        "prompt": "Advertising poster style {prompt} . Professional, modern, product-focused, commercial, eye-catching, highly detailed",
+        "c_prompt": "广告海报风格 {prompt} . 专业，现代，产品为中心，商业，引人注目，高度详细",
+        "negative_prompt": "noisy, blurry, amateurish, sloppy, unattractive",
+        "c_negative_prompt": "嘈杂，模糊，业余，马虎，不吸引人"
+    },
+    {
+        "name": "ads-automotive",
+        "c_name": "ads-汽车",
+        "prompt": "Automotive advertisement style {prompt} . Sleek, dynamic, professional, commercial, vehicle-focused, high-resolution, highly detailed",
+        "c_prompt": "汽车广告风格 {prompt} . 流畅，动态，专业，商业，以车为中心，高分辨率，高度详细",
+        "negative_prompt": "noisy, blurry, unattractive, sloppy, unprofessional",
+        "c_negative_prompt": "嘈杂，模糊，不吸引人，马虎，不专业"
+    },
+    {
+        "name": "ads-corporate",
+        "c_name": "ads-企业",
+        "prompt": "Corporate branding style {prompt} . Professional, clean, modern, sleek, minimalist, business-oriented, highly detailed",
+        "c_prompt": "企业品牌风格 {prompt} . 专业，干净，现代，流畅，简约，以商业为导向，高度详细",
+        "negative_prompt": "noisy, blurry, grungy, sloppy, cluttered, disorganized",
+        "c_negative_prompt": "嘈杂，模糊，肮脏，马虎，杂乱，无组织"
+    },
+    {
+        "name": "ads-fashion editorial",
+        "c_name": "ads-时尚编辑",
+        "prompt": "Fashion editorial style {prompt} . High fashion, trendy, stylish, editorial, magazine style, professional, highly detailed",
+        "c_prompt": "时尚编辑风格 {prompt} . 高级时尚，潮流，时尚，编辑，杂志风格，专业，高度详细",
+        "negative_prompt": "outdated, blurry, noisy, unattractive, sloppy",
+        "c_negative_prompt": "过时，模糊，嘈杂，不吸引人，马虎"
+    },
+    {
+        "name": "ads-food photography",
+        "c_name": "ads-食品摄影",
+        "prompt": "Food photography style {prompt} . Appetizing, professional, culinary, high-resolution, commercial, highly detailed",
+        "c_prompt": "食品摄影风格 {prompt} . 开胃，专业，烹饪，高分辨率，商业，高度详细",
+        "negative_prompt": "unappetizing, sloppy, unprofessional, noisy, blurry",
+        "c_negative_prompt": "不开胃，马虎，不专业，嘈杂，模糊"
+    },
+    {
+        "name": "ads-luxury",
+        "c_name": "ads-奢侈品",
+        "prompt": "Luxury product style {prompt} . Elegant, sophisticated, high-end, luxurious, professional, highly detailed",
+        "c_prompt": "奢侈品风格 {prompt} . 优雅，复杂，高端，豪华，专业，高度详细",
+        "negative_prompt": "cheap, noisy, blurry, unattractive, amateurish",
+        "c_negative_prompt": "便宜，嘈杂，模糊，不吸引人，业余"
+    },
+    {
+        "name": "ads-real estate",
+        "c_name": "ads-房地产",
+        "prompt": "Real estate photography style {prompt} . Professional, inviting, well-lit, high-resolution, property-focused, commercial, highly detailed",
+        "c_prompt": "房地产摄影风格 {prompt} . 专业，温馨，光线充足，高分辨率，以房产为中心，商业，高度详细",
+        "negative_prompt": "dark, blurry, unappealing, noisy, unprofessional",
+        "c_negative_prompt": "暗，模糊，不吸引人，嘈杂，不专业"
+    },
+    {
+        "name": "ads-retail",
+        "c_name": "ads-零售",
+        "prompt": "Retail packaging style {prompt} . Vibrant, enticing, commercial, product-focused, eye-catching, professional, highly detailed",
+        "c_prompt": "零售包装风格 {prompt} . 充满活力，诱人，商业，以产品为中心，引人注目，专业，高度详细",
+        "negative_prompt": "noisy, blurry, amateurish, sloppy, unattractive",
+        "c_negative_prompt": "嘈杂，模糊，业余，马虎，不吸引人"
+    }
+]
